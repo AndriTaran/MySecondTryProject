c949a4bb1615681e18cc3397131e125414dd730c
AndriTaran <andrey.taran@evernetica.com>
2021-08-12 17:47:53 +0300

    add validation schema for login

---
+++ src/assets/validation/validationSchema.js
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+export default {
+  LoginSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Required"),
+    password: Yup.string()
+      .min(8, "Must be more than 8 characters")
+      .matches(/[A-Z]+/g, 'Must be at least one uppercase letter')
+      .matches(/[a-z]+/g, 'Must be at least one lowercase letter')
+      .matches(/[^\s\w\d]+|_+/g, 'Must be at least one symbol letter')
+      .required("Required")
+  }),
+
+}
