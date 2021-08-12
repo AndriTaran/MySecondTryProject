58b819c702ef9cab21ddd87872e91ea6122498f8
AndriTaran <andrey.taran@evernetica.com>
2021-08-12 17:47:32 +0300

    add KeyboardAvoidingView component

---
+++ src/common/simpleComponents/KeyboardAvoidingView.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+import {dimensionStyles} from "./Mixins";
+
+export const KeyboardAvoidingView = styled.KeyboardAvoidingView`
+  ${dimensionStyles}
+`;
