9a4e6db630b42e352e4f7bcc2f6e19a6ec345706
AndriTaran <andrey.taran@evernetica.com>
2021-08-13 16:59:57 +0300

    fix form and add custom validator

--- src/Screens/AuthorizationScreen/AuthorizationScreen.js
+++ src/Screens/AuthorizationScreen/AuthorizationScreen.js
@@ -1,41 +1,54 @@
 import React from "react";
 import { Text, Block, KeyboardAvoidingView } from "../../common/simpleComponents/";
-import { Keyboard, Platform, TouchableWithoutFeedback } from "react-native";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import FormComponent from "./component/FormComponent";
 
 const AuthorizationScreen = () => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Block height={"100%"}
-             width={"100%"}
-             bc={'white'}
+      <Block
+        flex={1}
+        width={"100%"}
+        bc={"white"}
       >
-        <Block height={"40%"}
-               bc={"orange"}
-               justifyContent={"center"}
-               width={"100%"}
+        <Block
+          height={"40%"}
+          bc={"orange"}
+          justifyContent={"center"}
+          width={"100%"}
         >
           <Block
             justifyContent={"center"}
             position={"relative"}
           >
-            <Text color={"white"}
-                  fontSize={"120px"}>D
+            <Text
+              color={"white"}
+              fontSize={"120px"}
+            >D
             </Text>
             <Block
               position={"absolute"}
               top={"20px"}
               right={"-10px"}
             >
-              <Text fontSize={"30px"}
-                    color={"white"}>®
+              <Text
+                fontSize={"30px"}
+                color={"white"}
+              >®
               </Text>
             </Block>
           </Block>
         </Block>
-        <Block height={"100%"} width={"100%"}>
-          <KeyboardAvoidingView height={"100%"} width={"100%"} behavior={Platform.OS === "ios" ? "padding" : "padding"}>
+        <Block
+          width={"100%"}
+          flex={1}
+        >
+          <KeyboardAvoidingView
+            flex={1}
+            width={"100%"}
+            behavior={"padding"}
+          >
             <FormComponent />
           </KeyboardAvoidingView>
         </Block>
