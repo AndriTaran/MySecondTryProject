3611ba2e5ac412339414c74d24905720ca31fcc5
AndriTaran <andrey.taran@evernetica.com>
2021-08-16 11:51:12 +0300

    add header and button to WebViewScreen

--- src/Screens/WebViewScreen/WebViewScreen.js
+++ src/Screens/WebViewScreen/WebViewScreen.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import React from "react";
+import { WebView } from "react-native-webview";
+import { Header, ButtonGroup } from "../../common/combinedComponents";
+import { SafeAreaView } from "../../common/simpleComponents/SaveAreaView";
 
 const WebViewScreen = () => (
-   <WebView
-      source={{ uri: 'https://reactnative.dev/' }}
-      onError={(syntheticEvent) => {
-         const { nativeEvent } = syntheticEvent;
-         console.warn('WebView error: ', nativeEvent);
-      }}
-      onHttpError={(syntheticEvent) => {
-         const { nativeEvent } = syntheticEvent;
-         console.warn(
-            'Oops, something goes wrong. WebView received error status code: ',
-            nativeEvent.statusCode,
-         );
-      }}
-      originWhitelist={['https://*', 'git://*']}
-   />
-)
+   <SafeAreaView>
+      <Header />
+      <ButtonGroup text={"Web View"}/>
+      <WebView
+         source={{ uri: "https://reactnative.dev/" }}
+         onError={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.warn("WebView error: ", nativeEvent);
+         }}
+         onHttpError={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.warn(
+               "Oops, something goes wrong. WebView received error status code: ",
+               nativeEvent.statusCode,
+            );
+         }}
+         originWhitelist={["https://*", "git://*"]}
+      />
+   </SafeAreaView>
+);
 
-export default WebViewScreen
+export default WebViewScreen;
