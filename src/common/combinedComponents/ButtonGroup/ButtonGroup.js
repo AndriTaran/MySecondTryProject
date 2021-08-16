7b479c606e77b5626fe3b626fda5f2bf3ec94552
AndriTaran <andrey.taran@evernetica.com>
2021-08-16 12:27:22 +0300

    add goForwar and goBack functional for webView component

--- src/common/combinedComponents/ButtonGroup/ButtonGroup.js
+++ src/common/combinedComponents/ButtonGroup/ButtonGroup.js
@@ -3,18 +3,18 @@ import {Button} from "../../simpleComponents/Button";
 import {Text} from "../../simpleComponents/Text";
 import React from "react";
 
-const ButtonGroup = ({text}) =>(
+const ButtonGroup = ({text, goBack, goForward}) =>(
     <Block pt={'15px'}
            pb={'15px'}
            flexDirection={'row'}
            width={'100%'}
            justifyContent={'space-between'}>
-        <Button width={'80px'}>
-            <Text color={'blue'} fontSize={'20px'}>Cancel</Text>
+        <Button width={'80px'} onPress={goBack ? goBack : null}>
+            <Text color={'blue'} fontSize={'20px'}>{goBack ? "Back" : "Cancel"}</Text>
         </Button>
         <Text fontSize={'20px'} color={'black'}>{text}</Text>
-        <Button width={'80px'}>
-            <Text color={'blue'} fontSize={'20px'}>Confirm</Text>
+        <Button width={'80px'} onPress={goForward ? goForward : null}>
+            <Text color={'blue'} fontSize={'20px'}>{goForward ? "Forward" : "Confirm"}</Text>
         </Button>
     </Block>
 )
