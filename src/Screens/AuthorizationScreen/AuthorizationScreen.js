d8b21aaea524f3816757e6629707f994b82780fa
AndriTaran <andrey.taran@evernetica.com>
2021-08-17 16:55:18 +0300

    Add splash screen for adding personal image to load screen

--- src/Screens/AuthorizationScreen/AuthorizationScreen.js
+++ src/Screens/AuthorizationScreen/AuthorizationScreen.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SplashScreen from 'react-native-splash-screen'
 import { Text, Block, KeyboardAvoidingView } from "../../common/simpleComponents/";
 import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import FormComponent from "./component/FormComponent";
 
-const AuthorizationScreen = () => (
+const AuthorizationScreen = () => {
+   useEffect(()=>SplashScreen.hide(),[]);
+
+   return (
    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <Block
          flex={1}
@@ -52,6 +56,7 @@ const AuthorizationScreen = () => (
          </Block>
       </Block>
    </TouchableWithoutFeedback>
-);
+   )
+};
 
 export default AuthorizationScreen;
