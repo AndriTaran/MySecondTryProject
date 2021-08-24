fcd27a2fb1f63a74f34e0f52d64223b5f821ea91
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 19:02:42 +0300

    refactor image bar components

--- src/Screens/CameraScreen/components/ImageBar.js
+++ src/Screens/CameraScreen/components/ImageBar.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import PhotoItem from "./PhotoItem";
-import { Block } from "../../../common/simpleComponents/";
+import { View } from "../../../common/simpleComponents/";
 import { FlatList } from "react-native";
 
 const ImageBar = () => {
 
   const photos = useSelector(state => state.photo.photos);
 
-  const renderItem = ({ item }) => (
-    <PhotoItem source={item.uri || item.image} key={item.uri} />
-  );
+  const renderItem = ({ item }) => <PhotoItem source={item.uri || item.image} />;
 
   return (
-    <Block
+    <View
       height={"15%"}
+      width={"100%"}
       bc={"grey"}
       pt={"10px"}
     >
@@ -22,9 +21,9 @@ const ImageBar = () => {
         horizontal={true}
         data={photos}
         renderItem={renderItem}
-        keyExtractor={item => item.uri}
+        keyExtractor={item => `${item.uri || item.image}`}
       />
-    </Block>
+    </View>
   );
 };
 
