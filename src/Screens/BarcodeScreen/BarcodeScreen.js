f0815370b4c2b4c66afcf210b6a217f2103d893c
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 14:34:27 +0300

    fix barcode screen

--- src/Screens/BarcodeScreen/BarcodeScreen.js
+++ src/Screens/BarcodeScreen/BarcodeScreen.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Block, SafeAreaView, Text } from "../../common/simpleComponents/index";
+import React, { useEffect, useState } from "react";
+import { View, SafeAreaView, Text } from "../../common/simpleComponents/index";
 import { ButtonGroup, Footer, Header, WarehouseInfo } from "../../common/combinedComponents/index";
 import ScanningIcon from "../../assets/icons/scanningIcon.svg";
 
 const BarcodeScreen = () => {
+
   const [counter, setCounter] = useState(0);
   const [text, setText] = useState("Pull the trigger to start reading");
   const [error, setError] = useState(false);
 
-  function getRandomIntInclusive() {
+  const getRandomIntInclusive = () => {
     const randomIsTrue = Math.random() < 0.7;
     if (randomIsTrue) {
       setError(true);
@@ -18,7 +19,7 @@ const BarcodeScreen = () => {
       setCounter(currentValue => currentValue + 1);
       setText("Pull the trigger to stop reading");
     }
-  }
+  };
 
   useEffect(() => {
     let timerId = setTimeout(function tick() {
@@ -27,28 +28,28 @@ const BarcodeScreen = () => {
     }, 3000);
   }, []);
 
-
   return (
     <SafeAreaView>
-      <Block flexDirection={"column"} flex={1}>
+      <View flexDirection={"column"} flex={1}>
         <Header />
         <ButtonGroup text={"Summary"} />
         <WarehouseInfo number={246} label={9} storage={"2810-LAS VEGAS(TST)"} />
-        <Block
-          flex={1}
-          justifyContent={"space-around"}
-        >
-          {text === "Pull the trigger to start reading" ? <></> : <>
-            <Text fontSize={"130px"} color={"#013555"}>{counter}</Text>
-            <Block justifyContent={"space-around"}>
-            <ScanningIcon />
-              <Text fontSize={'20px'} pt={'15px'} color={'grey'}>Reading RFID...</Text>
-            </Block>
-          </>
+        <View flex={1} justifyContent={"space-around"}>
+          {text !== "Pull the trigger to start reading"
+            ?
+            <>
+              <Text fontSize={"130px"} color={"#013555"}>{counter}</Text>
+              <View justifyContent={"space-around"}>
+                <ScanningIcon />
+                <Text fontSize={"20px"} pt={"15px"} color={"grey"}>Reading RFID...</Text>
+              </View>
+            </>
+            :
+            null
           }
-        </Block>
+        </View>
         <Footer err={error} text={text} />
-      </Block>
+      </View>
     </SafeAreaView>
   );
 };
