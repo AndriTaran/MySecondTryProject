bc9d02be0eec2ab72b07cf670ee3958b6d4f89e1
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 19:06:49 +0300

    refactor item components

--- src/common/combinedComponents/Item/Item.js
+++ src/common/combinedComponents/Item/Item.js
@@ -1,31 +1,58 @@
 import React from "react";
-import {Block, Image, Text, Button} from "../../simpleComponents/";
+import { View, Image, Text, Button } from "../../simpleComponents/";
 
-const Item = ({color, size, brand, amount, picture, name, drag}) => (
-    <Button onPressIn={drag} flex={1} flexDirection={'row'} bc={'white'} activeOpacity={1} >
-        <Block flexDirection={'row'}
-               width={'100%'}
-               borderBottomCollor={'grey'}
-               pt={'20px'}
-               pb={'20px'}
-               pl={'10px'}
-               borderBottomWidth={'1px'}
+const Item = ({ color, size, brand, amount, picture, name, drag }) => (
+  <Button
+    onPressIn={drag}
+    flex={1}
+    flexDirection={"row"}
+    bc={"white"}
+    activeOpacity={1}
+  >
+    <View
+      flexDirection={"row"}
+      width={"100%"}
+      borderBottomCollor={"grey"}
+      pt={"20px"}
+      pb={"20px"}
+      pl={"10px"}
+      borderBottomWidth={"1px"}
+    >
+      <Image source={{ uri: picture }} height={"70px"} width={"50px"} />
+      <View
+        flexDirection={"column"}
+        alignItm={"flex-start"}
+        pl={"10px"}
+        flex={1}
+      >
+        <Text pt={"3px"} pb={"3px"} color={"black"}>
+          {name}
+        </Text>
+        <Text pt={"3px"} pb={"3px"} color={"black"}>
+          {color} {size}
+        </Text>
+        <Text pt={"3px"} pb={"3px"} color={"black"}>
+          {brand}
+        </Text>
+      </View>
+      <View flexDirection={"row"}>
+        <View
+          bc={"green"}
+          width={"100px"}
+          justifyContent={"center"}
+          borderRadius={"10px"}
+          flexDirection={"row"}
         >
-            <Image source={{uri: picture}}
-                   height={'70px'} width={'50px'}/>
-            <Block flexDirection={'column'} alignItm={'flex-start'} pl={'10px'} flex={1}>
-                <Text pt={'3px'} pb={'3px'} color={'black'}>{name}</Text>
-                <Text pt={'3px'} pb={'3px'} color={'black'}>{color} {size}</Text>
-                <Text pt={'3px'} pb={'3px'} color={'black'}>{brand}</Text>
-            </Block>
-            <Block flexDirection={'row'}>
-                <Block bc={'green'} width={'100px'} justifyContent={'center'} borderRadius={'10px'} flexDirection={'row'}>
-                    <Text color={'white'}>{amount}</Text>
-                </Block>
-                <Text color={'black'}> > </Text>
-            </Block>
-        </Block>
-    </Button>
-)
+          <Text color={"white"}>
+            {amount}
+          </Text>
+        </View>
+        <Text color={"black"}>
+          >
+        </Text>
+      </View>
+    </View>
+  </Button>
+);
 
-export default Item
+export default Item;
