7e457036e9121422a6c8b911e1b50f2fc3483938
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 14:03:44 +0300

    fix authorization screen

--- src/Screens/AuthorizationScreen/AuthorizationScreen.js
+++ src/Screens/AuthorizationScreen/AuthorizationScreen.js
@@ -1,62 +1,55 @@
 import React, { useEffect } from "react";
-import SplashScreen from 'react-native-splash-screen'
-import { Text, Block, KeyboardAvoidingView } from "../../common/simpleComponents/";
+import SplashScreen from "react-native-splash-screen";
 import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { Text, View, KeyboardAvoidingView } from "../../common/simpleComponents/";
 import FormComponent from "./component/FormComponent";
 
 const AuthorizationScreen = () => {
-   useEffect(()=>SplashScreen.hide(),[]);
 
-   return (
-   <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Block
-         flex={1}
-         width={"100%"}
-         bc={"white"}
+  useEffect(() => SplashScreen.hide(), []);
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View
+        flex={1}
+        width={"100%"}
+        bc={"white"}
       >
-         <Block
-            height={"40%"}
-            bc={"orange"}
+        <View
+          height={"40%"}
+          bc={"orange"}
+          justifyContent={"center"}
+          width={"100%"}
+        >
+          <View
             justifyContent={"center"}
-            width={"100%"}
-         >
-            <Block
-               justifyContent={"center"}
-               position={"relative"}
+            position={"relative"}
+          >
+            <Text color={"white"} fontSize={"120px"}>D</Text>
+            <View
+              position={"absolute"}
+              top={"20px"}
+              right={"-10px"}
             >
-               <Text
-                  color={"white"}
-                  fontSize={"120px"}
-               >D
-               </Text>
-               <Block
-                  position={"absolute"}
-                  top={"20px"}
-                  right={"-10px"}
-               >
-                  <Text
-                     fontSize={"30px"}
-                     color={"white"}
-                  >®
-                  </Text>
-               </Block>
-            </Block>
-         </Block>
-         <Block
-            width={"100%"}
+              <Text fontSize={"30px"} color={"white"}>®</Text>
+            </View>
+          </View>
+        </View>
+        <View
+          width={"100%"}
+          flex={1}
+        >
+          <KeyboardAvoidingView
             flex={1}
-         >
-            <KeyboardAvoidingView
-               flex={1}
-               width={"100%"}
-               behavior={"padding"}
-            >
-               <FormComponent />
-            </KeyboardAvoidingView>
-         </Block>
-      </Block>
-   </TouchableWithoutFeedback>
-   )
+            width={"100%"}
+            behavior={"padding"}
+          >
+            <FormComponent />
+          </KeyboardAvoidingView>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export default AuthorizationScreen;
