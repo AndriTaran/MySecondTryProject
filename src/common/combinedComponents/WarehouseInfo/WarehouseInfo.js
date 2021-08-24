d13ec8df14f26ba14209db76064a2acd7c856080
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 19:11:25 +0300

    refactor warehouse info components

--- src/common/combinedComponents/WarehouseInfo/WarehouseInfo.js
+++ src/common/combinedComponents/WarehouseInfo/WarehouseInfo.js
@@ -1,65 +1,74 @@
-import { Block } from "../../simpleComponents/Block";
-import BoxIcon from "../../../assets/icons/closed-cardboard-box.svg";
-import { Text } from "../../simpleComponents/Text";
-import LabelIcon from "../../../assets/icons/price-label-svgrepo-com.svg";
 import React from "react";
+import { View, Text } from "../../simpleComponents/";
+import BoxIcon from "../../../assets/icons/closed-cardboard-box.svg";
+import LabelIcon from "../../../assets/icons/price-label-svgrepo-com.svg";
 
 const WarehouseInfo = ({ number, label, storage }) => (
-  <Block pt={"10px"}
-         pb={"10px"}
-         bc={"#E5E5E5"}
-         borderBottomCollor={"grey"}
-         borderTopCollor={"grey"}
-         flexDirection={"row"}
-         width={"100%"}
-         borderBottomWidth={"1px"}
-         borderTopWidth={"1px"}>
+  <View
+    pt={"10px"}
+    pb={"10px"}
+    bc={"#E5E5E5"}
+    borderBottomCollor={"grey"}
+    borderTopCollor={"grey"}
+    flexDirection={"row"}
+    width={"100%"}
+    borderBottomWidth={"1px"}
+    borderTopWidth={"1px"}
+  >
     {storage ?
-      <Block justifyContent={"space-between"} flexDirection={'row'}>
-        <Block flex={1} alignItm={'flex-start'}>
-          <Text pl={"5px"} color={"black"}>{storage}</Text>
-        </Block>
-        <Block
-          justifyContent={"flex-end"}
-          pr={'10px'}
-          flexDirection={"row"}>
-          <BoxIcon width={20}
-                   height={20}
-                   fill={"black"} />
-          <Text pl={"5px"} color={"black"}>{number}</Text>
-        </Block>
-        <Block
+      <View justifyContent={"space-between"} flexDirection={"row"}>
+        <View flex={1} alignItm={"flex-start"}>
+          <Text pl={"5px"} color={"black"}>
+            {storage}
+          </Text>
+        </View>
+        <View
           justifyContent={"flex-end"}
           pr={"10px"}
-          flexDirection={"row"}>
-          <LabelIcon width={"20px"}
-                     height={"20px"}
-                     fill={"black"} />
-          <Text pl={"5px"} color={"black"}>{label}</Text>
-        </Block>
-      </Block>
+          flexDirection={"row"}
+        >
+          <BoxIcon width={20} height={20} fill={"black"} />
+          <Text pl={"5px"} color={"black"}>
+            {number}
+          </Text>
+        </View>
+        <View
+          justifyContent={"flex-end"}
+          pr={"10px"}
+          flexDirection={"row"}
+        >
+          <LabelIcon width={"20px"} height={"20px"} fill={"black"} />
+          <Text pl={"5px"} color={"black"}>
+            {label}
+          </Text>
+        </View>
+      </View>
       :
       <>
-        <Block width={"50%"}
-               justifyContent={"flex-start"}
-               pl={"10px"}
-               flexDirection={"row"}>
-          <BoxIcon width={20}
-                   height={20}
-                   fill={"black"} />
-          <Text pl={"5px"} color={"black"}>{number}</Text>
-        </Block>
-        <Block width={"50%"}
-               justifyContent={"flex-end"}
-               pr={"10px"}
-               flexDirection={"row"}>
-          <LabelIcon width={"20px"}
-                     height={"20px"}
-                     fill={"black"} />
-          <Text pl={"5px"} color={"black"}>{label}</Text>
-        </Block>
+        <View
+          width={"50%"}
+          justifyContent={"flex-start"}
+          pl={"10px"}
+          flexDirection={"row"}
+        >
+          <BoxIcon width={20} height={20} fill={"black"} />
+          <Text pl={"5px"} color={"black"}>
+            {number}
+          </Text>
+        </View>
+        <View
+          width={"50%"}
+          justifyContent={"flex-end"}
+          pr={"10px"}
+          flexDirection={"row"}
+        >
+          <LabelIcon width={"20px"} height={"20px"} fill={"black"} />
+          <Text pl={"5px"} color={"black"}>
+            {label}
+          </Text>
+        </View>
       </>}
-  </Block>
+  </View>
 );
 
 export default WarehouseInfo;
