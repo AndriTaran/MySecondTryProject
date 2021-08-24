a864030758333482a6fbd5047580dddcb0ebec44
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 18:48:33 +0300

    refactor notification screen

--- src/Screens/NotificationScreen/NotificationScreen.js
+++ src/Screens/NotificationScreen/NotificationScreen.js
@@ -1,60 +1,65 @@
 import React from "react";
 import { localNotification, scheduledLocalNotification } from "../../services/Notification/LocalPushController";
-import { Block, Text, Button } from "../../common/simpleComponents";
+import { View, Text, Button } from "../../common/simpleComponents";
 
 const NotificationScreen = () => {
 
-   return (
-      <Block flex={1} justifyContent={'center'}>
-         <Text fontSize={'20px'} mb={'20px'}>
-            Example app react-native-push-notification
-         </Text>
-         <Button
-            border={'1px solid black'}
-            margin={'5px'}
-            padding={'5px'}
-            backgroundColor={'#AAAAAA'}
-            borderRadius={'5px'}
-            width={"70%"}
-            onPress={() => localNotification()}>
-            <Text>Local Notification (now)</Text>
-         </Button>
-         <Button
-            border={'1px solid black'}
-            margin={'5px'}
-            padding={'5px'}
-            backgroundColor={'#AAAAAA'}
-            borderRadius={'5px'}
-            width={"70%"}
-            onPress={() => localNotification("sample.mp3")}>
-            <Text>Local Notification with sound (now)</Text>
-         </Button>
-         <Button
-            border={'1px solid black'}
-            margin={'5px'}
-            padding={'5px'}
-            backgroundColor={'#AAAAAA'}
-            borderRadius={'5px'}
-            width={"70%"}
-            onPress={() =>
-               scheduledLocalNotification()
-            }>
-            <Text>Schedule Notification in 30s</Text>
-         </Button>
-         <Button
-            border={'1px solid black'}
-            margin={'5px'}
-            padding={'5px'}
-            backgroundColor={'#AAAAAA'}
-            borderRadius={'5px'}
-            width={"70%"}
-            onPress={() => scheduledLocalNotification("sample.mp3")}>
-            <Text>Schedule Notification with sound in 30s</Text>
-         </Button>
+  const handlePressLocalSilent = () => localNotification();
+  const handlePressLocal = () => localNotification("sample.mp3");
+  const handlePressScheduleSilent = () => scheduledLocalNotification();
+  const handlePressSchedule = () => scheduledLocalNotification("sample.mp3");
 
-      </Block>
-   );
+  return (
+    <View flex={1} justifyContent={"center"}>
+      <Text fontSize={"20px"} mb={"20px"}>
+        Example app react-native-push-notification
+      </Text>
+      <Button
+        border={"1px solid black"}
+        margin={"5px"}
+        padding={"5px"}
+        backgroundColor={"#AAAAAA"}
+        borderRadius={"5px"}
+        width={"70%"}
+        onPress={handlePressLocalSilent}
+      >
+        <Text>Local Notification (now)</Text>
+      </Button>
+      <Button
+        border={"1px solid black"}
+        margin={"5px"}
+        padding={"5px"}
+        backgroundColor={"#AAAAAA"}
+        borderRadius={"5px"}
+        width={"70%"}
+        onPress={handlePressLocal}
+      >
+        <Text>Local Notification with sound (now)</Text>
+      </Button>
+      <Button
+        border={"1px solid black"}
+        margin={"5px"}
+        padding={"5px"}
+        backgroundColor={"#AAAAAA"}
+        borderRadius={"5px"}
+        width={"70%"}
+        onPress={handlePressScheduleSilent}
+      >
+        <Text>Schedule Notification in 30s</Text>
+      </Button>
+      <Button
+        border={"1px solid black"}
+        margin={"5px"}
+        padding={"5px"}
+        backgroundColor={"#AAAAAA"}
+        borderRadius={"5px"}
+        width={"70%"}
+        onPress={handlePressSchedule}
+      >
+        <Text>Schedule Notification with sound in 30s</Text>
+      </Button>
+    </View>
+  );
 };
-
 
 export default NotificationScreen;
