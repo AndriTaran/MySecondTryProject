d8eb368e48c43b8bf66b21cf18586f61a93c6723
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 19:09:04 +0300

    refactor navigation list components

--- src/common/combinedComponents/NavigationList/NavigationList.js
+++ src/common/combinedComponents/NavigationList/NavigationList.js
@@ -14,57 +14,56 @@ import NotificationScreen from "../../../Screens/NotificationScreen/Notification
 const Drawer = createDrawerNavigator();
 
 const NavigationList = () => {
-   const navigation = useNavigation()
-   const goToPage = ( notification ) => {
-      navigation.navigate(notification.data.screen)
-   }
-   useEffect(() => {
-      PushNotification.configure({
-         onNotification: function(notification) {
-            goToPage(notification)
-         },
-         popInitialNotification: true,
-         requestPermissions: true
-      }, )
-      PushNotification.createChannel(
-         {
-            channelId: "default-channel-id", // (required)
-            channelName: `Default channel`, // (required)
-            channelDescription: "A default channel", // (optional) default: undefined.
-            soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-            importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-            vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
-         },
-         (created) => console.log(`createChannel 'default-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
-      );
-      PushNotification.createChannel(
-         {
-            channelId: "sound-channel-id", // (required)
-            channelName: `Sound channel`, // (required)
-            channelDescription: "A sound channel", // (optional) default: undefined.
-            soundName: "sample.mp3", // (optional) See `soundName` parameter of `localNotification` function
-            importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-            vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
-         },
-         (created) => console.log(`createChannel 'sound-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
-      );
+  const navigation = useNavigation();
+  const goToPage = (notification) => {
+    navigation.navigate(notification.data.screen);
+  };
+  useEffect(() => {
+    PushNotification.configure({
+      onNotification: (notification) => {
+        goToPage(notification);
+      },
+      popInitialNotification: true,
+      requestPermissions: true,
+    });
+    PushNotification.createChannel(
+      {
+        channelId: "default-channel-id",
+        channelName: `Default channel`,
+        channelDescription: "A default channel",
+        soundName: "default",
+        importance: Importance.HIGH,
+        vibrate: true,
+      },
+    );
+    PushNotification.createChannel(
+      {
+        channelId: "sound-channel-id",
+        channelName: `Sound channel`,
+        channelDescription: "A sound channel",
+        soundName: "sample.mp3",
+        importance: Importance.HIGH,
+        vibrate: true,
+      },
+    );
 
-   }, []);
+  }, []);
 
-   return (
-      <Drawer.Navigator initialRouteName="LOGIN" screenOptions={{
-         headerShown: false,
-      }}>
-         <Drawer.Screen name="Home" component={HomeScreenMain} />
-         <Drawer.Screen name="Camera" component={CameraScreen} />
-         <Drawer.Screen name="Maps" component={MapsScreen} />
-         <Drawer.Screen name="LOGIN" component={AuthorizationScreen} />
-         <Drawer.Screen name="BarcodeScreen" component={BarcodeScreen} />
-         <Drawer.Screen name='WebViewScreen' component={WebViewScreen}/>
-         <Drawer.Screen name="Chart" component={ChartScreen}/>
-         <Drawer.Screen name='Notification' component={NotificationScreen}/>
-      </Drawer.Navigator>
-   );
+  return (
+    <Drawer.Navigator
+      initialRouteName="LOGIN"
+      screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreenMain} />
+      <Drawer.Screen name="Camera" component={CameraScreen} />
+      <Drawer.Screen name="Maps" component={MapsScreen} />
+      <Drawer.Screen name="LOGIN" component={AuthorizationScreen} />
+      <Drawer.Screen name="BarcodeScreen" component={BarcodeScreen} />
+      <Drawer.Screen name="WebViewScreen" component={WebViewScreen} />
+      <Drawer.Screen name="Chart" component={ChartScreen} />
+      <Drawer.Screen name="Notification" component={NotificationScreen} />
+    </Drawer.Navigator>
+  );
 };
 
 export default NavigationList;
