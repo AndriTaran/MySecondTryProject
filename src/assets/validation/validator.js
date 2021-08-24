50c7ebd699aa897f5ab2b17eb970c283d4b19c73
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 19:12:30 +0300

    refactor validator func

--- src/assets/validation/validator.js
+++ src/assets/validation/validator.js
@@ -1,7 +1,7 @@
 const validator = (schema) => (values) => schema.validate(values, {
   abortEarly: false,
   strict: false,
-}).then(() => ({})).catch(({inner}) =>  inner.reduce((memo, {path, message}) => ({
+}).catch(({inner}) =>  inner.reduce((memo, {path, message}) => ({
   ...memo,
   [path]: (memo[path] || []).concat(message),
 }), {}))
