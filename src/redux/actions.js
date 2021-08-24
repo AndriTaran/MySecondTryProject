df9b36742e582a9b978e65353239c69d5a1aa186
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 18:50:10 +0300

    refactor action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,9 @@
-import { ADD_PHOTO, GET_PHOTO, DELETE_PHOTO } from "./types";
+import { ADD_PHOTO, GET_PHOTO } from "./types";
 
 export function getPhoto() {
   return dispatch => dispatch({ type: GET_PHOTO });
 }
 
 export function addPhoto(data) {
-  console.log('new',data.uri || data.image)
   return dispatch => dispatch({ type: ADD_PHOTO, payload: data });
 }
