aec50e8ec467f319e44eb64c81b2d16c81296991
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 18:56:53 +0300

    refactor fingerprint components

--- src/common/combinedComponents/FingerPrintButton/FingerPrintButton.js
+++ src/common/combinedComponents/FingerPrintButton/FingerPrintButton.js
@@ -1,76 +1,63 @@
-import React, {useEffect, useRef, useState} from 'react';
-import FingerprintScanner from 'react-native-fingerprint-scanner';
-import {AppState} from 'react-native';
-import {Button, Text} from '../../simpleComponents';
-import FingerprintPopup from '../FingerprintPopup/FingerprintPopup.component';
+import React, { useEffect, useState } from "react";
+import FingerprintScanner from "react-native-fingerprint-scanner";
+import { AppState } from "react-native";
+import { Button, Text } from "../../simpleComponents";
+import FingerprintPopup from "../FingerprintPopup/FingerprintPopup.component";
 import I18n from "../../../assets/locales/i18n";
 
-
 const FingerprintButton = () => {
-  const appState = useRef(AppState.currentState);
   const [errorMessage, setErrorMessage] = useState();
-   const [biometric, setBiometric] = useState();
-   const [popupShowed, setPopupShowed] = useState(false);
-   const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  const [biometric, setBiometric] = useState();
+  const [popupShowed, setPopupShowed] = useState(false);
+  const [appStateVisible, setAppStateVisible] = useState(AppState.currentState);
 
-   useEffect(() => {
-      AppState.addEventListener('change', handleAppStateChange);
-      detectFingerprintAvailable();
-      return () => AppState.removeEventListener('change', handleAppStateChange);
-   }, [handleAppStateChange]);
+  const handleFingerprintShowed = () => setPopupShowed(true);
 
-   const handleFingerprintShowed = () => {
-      setPopupShowed(true);
-   };
+  const handleFingerprintDismissed = () => setPopupShowed(false);
 
-   const handleFingerprintDismissed = () => {
-      setPopupShowed(false);
-   };
+  const detectFingerprintAvailable = () =>
+    FingerprintScanner
+      .isSensorAvailable()
+      .catch(error => {
+        setErrorMessage(error.message);
+        setBiometric(error.biometric);
+      });
 
-   const detectFingerprintAvailable = () => {
-      FingerprintScanner
-         .isSensorAvailable()
-         .catch(error => {
-            setErrorMessage(error.message);
-            setBiometric(error.biometric);
-         });
-   };
+  const handleAppStateChange = (nextAppState) => {
 
-   const handleAppStateChange = (nextAppState) => {
-      if (appStateVisible.match(/inactive|background/) && nextAppState === 'active') {
-         FingerprintScanner.release();
-         detectFingerprintAvailable();
-      }
-      setAppStateVisible(nextAppState);
-   };
+    if (appStateVisible.match(/inactive|background/) && nextAppState === "active") {
+      FingerprintScanner.release();
+      detectFingerprintAvailable().then(r => console.warn("fingerprint scanner is not available", r));
+    }
+    setAppStateVisible(nextAppState);
+  };
+  useEffect(() => {
+    AppState.addEventListener("change", handleAppStateChange);
+    return () => AppState.removeEventListener("change", handleAppStateChange);
+  }, [handleAppStateChange]);
 
-   return (
-      <Button
-         mt={'10px'}
-         color={'black'}
-         height={'30px'}
-         justifyContent={'center'}
-         onPress={handleFingerprintShowed}
+  return (
+    <Button
+      mt={"10px"}
+      color={"black"}
+      height={"30px"}
+      justifyContent={"center"}
+      onPress={handleFingerprintShowed}
+    >
+      <Text
+        fontSize={"20px"}
+        color={"blue"}
       >
-         <Text
-            fontSize={'20px'}
-            color={'blue'}
-         >{I18n.t('logWithBiometric')}
-         </Text>
-         {
-            popupShowed && (
-               <FingerprintPopup
-                  handlePopupDismissed={handleFingerprintDismissed}
-               />
-            )
-         }
-         {errorMessage && (
-            <Text>
-               {errorMessage} {biometric}
-            </Text>
-         )}
-      </Button>
-   );
+        {I18n.t("logWithBiometric")}
+      </Text>
+      {
+        popupShowed && (<FingerprintPopup handlePopupDismissed={handleFingerprintDismissed} />)
+      }
+      {
+        errorMessage && (<Text>{errorMessage} {biometric}</Text>)
+      }
+    </Button>
+  );
 };
 
 export default FingerprintButton;
--- src/common/combinedComponents/FingerprintPopup/FingerprintPopup.component.ios.js
+++ src/common/combinedComponents/FingerprintPopup/FingerprintPopup.component.ios.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Alert } from "react-native";
 import FingerprintScanner from "react-native-fingerprint-scanner";
 
-class FingerprintPopup extends Component {
+const FingerprintPopup = (props) => {
 
-   componentDidMount() {
-      FingerprintScanner
-         .authenticate({ description: "Scan your fingerprint on the device scanner to continue" })
-         .then(() => {
-            this.props.handlePopupDismissed();
-            Alert.alert("Authenticated successfully");
-         })
-         .catch((error) => {
-            this.props.handlePopupDismissed();
-            Alert.alert(error.message);
-         });
-   }
+  useEffect(() => FingerprintScanner
+    .authenticate({ description: "Scan your fingerprint on the device scanner to continue" })
+    .then(() => {
+      props.handlePopupDismissed();
+      Alert.alert("Authenticated successfully");
+    })
+    .catch((error) => {
+      props.handlePopupDismissed();
+      Alert.alert(error.message);
+    }), []);
 
-   render() {
-      return false;
-   }
-}
-
+  return false;
+};
 
 export default FingerprintPopup;
