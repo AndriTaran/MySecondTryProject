0508e5f46243babe20cc09ef0e48e438d7a35b95
AndriTaran <andrey.taran@evernetica.com>
2021-08-24 18:51:41 +0300

    refactor button group

--- src/common/combinedComponents/ButtonGroup/ButtonGroup.js
+++ src/common/combinedComponents/ButtonGroup/ButtonGroup.js
@@ -1,22 +1,31 @@
-import {Block} from "../../simpleComponents/Block";
-import {Button} from "../../simpleComponents/Button";
-import {Text} from "../../simpleComponents/Text";
+import { View } from "../../simpleComponents/View";
+import { Button } from "../../simpleComponents/Button";
+import { Text } from "../../simpleComponents/Text";
 import React from "react";
 
-const ButtonGroup = ({text, goBack, goForward}) =>(
-    <Block pt={'15px'}
-           pb={'15px'}
-           flexDirection={'row'}
-           width={'100%'}
-           justifyContent={'space-between'}>
-        <Button width={'80px'} onPress={goBack ? goBack : null}>
-            <Text color={'blue'} fontSize={'20px'}>{goBack ? "Back" : "Cancel"}</Text>
-        </Button>
-        <Text fontSize={'20px'} color={'black'}>{text}</Text>
-        <Button width={'80px'} onPress={goForward ? goForward : null}>
-            <Text color={'blue'} fontSize={'20px'}>{goForward ? "Forward" : "Confirm"}</Text>
-        </Button>
-    </Block>
-)
+const ButtonGroup = ({ text, goBack, goForward }) => (
 
-export default ButtonGroup
+  <View
+    pt={"15px"}
+    pb={"15px"}
+    flexDirection={"row"}
+    width={"100%"}
+    justifyContent={"space-between"}
+  >
+    <Button width={"80px"} onPress={goBack ? goBack : null}>
+      <Text color={"blue"} fontSize={"20px"}>
+        {goBack ? "Back" : "Cancel"}
+      </Text>
+    </Button>
+    <Text fontSize={"20px"} color={"black"}>
+      {text}
+    </Text>
+    <Button width={"80px"} onPress={goForward ? goForward : null}>
+      <Text color={"blue"} fontSize={"20px"}>
+        {goForward ? "Forward" : "Confirm"}
+      </Text>
+    </Button>
+  </View>
+);
+
+export default ButtonGroup;
